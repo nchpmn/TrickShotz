5ab601eadfa9671516838596eacfab33c24c584f
nchpmn <nathan@nchp.mn>
2023-11-02 12:01:07 +1100

    Build initial ball and plank Classes

--- TrickShotz.ino
+++ TrickShotz.ino
@@ -7,15 +7,58 @@
 #include <Arduboy2.h>
 Arduboy2 a;
 
-const int gravity = 3;
-int ballX = 10;
-int ballY = 5;
-int ballRadius = 3;
+class Ball{
+    public:
+        float x, y; // Position
+        float vx, vy; // Velocity (X and Y components)
+        uint8_t size; // Radius of ball
+
+        Ball(float startX, float startY, float startVX, float startVY, uint8_t startSize) {
+            x = startX;
+            y = startY;
+            vx = startVX;
+            vy = startVY;
+            size = startSize;
+        }
+
+        void update() {
+            // Update the ball's position from it's velocity values
+            x += vx;
+            y += vy;
+        }
+
+        void draw() {
+            a.fillCircle(static_cast<int16_t>(round(x)), static_cast<int16_t>(round(y)), size, WHITE);
+        }
+};
+Ball newBall(10,20,0.5,0.3,2);
+
+class Plank {
+    public:
+        int16_t x1, y1; // Coordinates of one end
+        int16_t x2, y2; // Coordinates of the other end
+        uint8_t thickness; // Thickness of the plank
+
+        Plank(int16_t startX1, int16_t startY1, int16_t startX2, int16_t startY2, uint8_t startThickness) {
+            x1 = startX1;
+            y1 = startY1;
+            x2 = startX2;
+            y2 = startY2;
+            thickness = startThickness;
+        }
+
+        void draw() {
+            for (uint8_t i = 0; i < thickness; i++) {
+                a.drawLine(x1+i,y1+i, x2+i,y2+i, WHITE);
+            }
+        }
+};
+Plank newPlank(5,30,135,40,4);
+
 
 void setup() {
     a.begin();
     a.setFrameRate(60);
-    a.display();
     a.initRandomSeed();
     a.clear();
 }
@@ -27,9 +70,10 @@ void loop() {
     a.pollButtons();
     a.clear();
 
-    a.fillCircle(ballX, ballY, ballRadius, WHITE);
+    newBall.draw();
+    newBall.update();
 
-    ballY += gravity;
+    newPlank.draw();
     
     a.display();
 }

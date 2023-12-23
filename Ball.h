958c8fbc2559bf54e36325c5a74cc710b6db22c5
nchpmn <nathan@nchp.mn>
2023-12-23 12:31:12 +1100

    Add draw() functions to Ball.h

--- Ball.h
+++ Ball.h
@@ -3,5 +3,72 @@
 
 #include <Arduboy2.h>
 
+class Ball {
+public:
+    Pos<float> position = Pos<float>(64, 32);
+    Vector velocity = Vector(0, 0);
+
+    // Radius of ball
+    uint8_t size = 2;
+
+    // Index of launchPowerLevels[] array
+    uint8_t launchPower = 2;
+    // Launch angle 0 to 359
+    uint16_t launchAngle = 25;
+    // Millisecond timer for ball offscreen
+    uint8_t offscreenTimer = 0;
+
+    // Default constructor with default values
+    Ball() = default;
+
+    Ball(float startX, float startY) : position(startX, startY) {
+    };
+
+    // MOVE COLLIDE FUNCTIONS HERE? - I.E.
+    // 1. Move Ball
+    // 2. Check collide with Planks (and update ball)
+    // 3. Check collie with Goal (and upload level complete)
+    // 4. Draw Ball
+
+    void draw() const {
+        drawBall();
+        if (levelState == LevelState::Aim) {
+            drawBallAiming();
+        }
+    }
+
+private:
+    float launchPowerLevels[5] = {0.5, 1, 1.5, 2, 2.5};
+
+    void drawBall() const {
+        a.fillCircle(round(position.x), round(position.y), size, WHITE);
+    }
+
+    void drawBallAiming() const {
+        // Draw a dotted line projecting where the ball will travel
+        uint8_t lineLength = 25;
+        int16_t adjustedAngle = launchAngle - 90;
+        if (adjustedAngle < 0) {
+            adjustedAngle += 360;
+        }
+
+        Pos<float> simPosition = position;
+        Vector simVelocity = {
+            launchPowerLevels[launchPower - 1] * cos(radians(adjustedAngle)),
+            launchPowerLevels[launchPower - 1] * sin(radians(adjustedAngle)) + GRAVITY
+        };
+
+        for (int8_t t = 0; t < lineLength; t++) {
+            simPosition += simVelocity;
+
+            if (t % 3 == 0) {
+                a.drawPixel(round(simPosition.x), round(simPosition.y), WHITE);
+            }
+        }
+    }
+
+
+};
+
 
 #endif

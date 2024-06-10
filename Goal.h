ca495f229448c4602ad126292cbd1a40d769dd3a
nchpmn <nathan@nchp.mn>
2024-06-10 11:12:06 +1000

    Implement Pos into Goal

--- Goal.h
+++ Goal.h
@@ -3,7 +3,7 @@
 
 class Goal {
 public:
-    Goal(int x, int y, int radius) : x(x), y(y), radius(radius) {}
+    Goal(int x, int y, int radius) : position(x, y), radius(radius) {}
 
     void draw() {
         static float rotationAngle = 0.0;
@@ -13,10 +13,10 @@ public:
             float angle1 = TWO_PI / dashNumber * i + rotationAngle;
             float angle2 = TWO_PI / dashNumber * (i + 1) + rotationAngle;
 
-            int x1 = x + radius * cos(angle1);
-            int y1 = y + radius * sin(angle1);
-            int x2 = x + radius * cos(angle2);
-            int y2 = y + radius * sin(angle2);
+            int x1 = getX() + radius * cos(angle1);
+            int y1 = getY() + radius * sin(angle1);
+            int x2 = getX() + radius * cos(angle2);
+            int y2 = getY() + radius * sin(angle2);
 
             // Draw each dash as a line segment
             a.drawLine(x1, y1, x2, y2, WHITE);
@@ -29,21 +29,19 @@ public:
         }
     }
 
-    int getX() const { return x; }
-    int getY() const { return y; }
+    int getX() const { return position.x; }
+    int getY() const { return position.y; }
     int getRadius() const { return radius; }
     void setPosition(uint8_t newX, uint8_t newY) {
-        x = newX;
-        y = newY;
+        position.set(newX,newY);
     }
     void setRadius(uint8_t newRadius) { radius = newRadius; }
 
 
 private:
-    int x;
-    int y;
-    int radius;
-    int dashNumber = 17;
+    Pos position;
+    uint8_t radius;
+    uint8_t dashNumber = 17;
 
 };
 
